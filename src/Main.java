0f3e6035b1ebc033c557e030cd0d60b95bb7af78
Andy Verh <bepx@inbox.ru>
2022-09-25 23:58:20 +0600

    аут в игнор и корректировка Мэйна еще раз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        SalesManager Olya = new SalesManager(new int[]{3, 5, 8}); //оля совершила за раб день 3 сделки на 3, 5 и 8$
+        System.out.println(Olya.max()); //покажи самую прибыльную продажу Оли
     }
 }
