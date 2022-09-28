41d16316b38be470486cacd08512fb3ff2c4f559
Andy Verh <bepx@inbox.ru>
2022-09-29 00:48:37 +0600

    добавил обрезаный расчет среднего значения продаж

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,27 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int min() {
+        int min = 0;
+        for (int sale : sales) {
+            if (sale <= min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int averageCuted() {
+        int sumSales = 0;
+        int sumIndx = 0;
+        int avrCtd = 0;
+        for (int i = 0; i < sales.length; i++) {
+            sumSales += sales[i];
+            sumIndx ++;
+        }
+        sumIndx = sumIndx - 2;
+        avrCtd = (sumSales - max() - min()) / sumIndx;
+        return avrCtd;
+    }
 }
